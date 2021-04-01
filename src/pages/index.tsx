53664c2e5a3ecf466bf4f82576cefbbfa740e7f3
Pedro Caires <pedro.caires@iteris.com.br>
2021-04-01 00:07:40 -0300

    feat: ssr e ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import {GetStaticProps} from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    ammount: number,
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +24,56 @@ export default function Home() {
           <h1>New about the <span>React</span> world</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.ammount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1IbFghHvoyX4mOt5zd4udwVq', {
+//     expand: ['product']
+//   });
+
+//   const product = {
+//     priceId: price.id,
+//     ammount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format(price.unit_amount / 100),
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const price = await stripe.prices.retrieve('price_1IbFghHvoyX4mOt5zd4udwVq', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    ammount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  const oneDay = 60 * 60 *24; 
+  return {
+    props: {
+      product
+    },
+    revalidate: oneDay
+  }
 }
