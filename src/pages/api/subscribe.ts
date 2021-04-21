0026244e89c7698a5721ff3305c5b52e2217e024
Pedro Caires <pedro.caires@iteris.com.br>
2021-04-21 00:42:14 -0300

    feat: evitando duplicados

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,83 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import {query as q} from 'faunadb';
 import { stripe } from "../../services/stripe";
 const { ENV_URL } = process.env;
+
+type User = {
+    ref: {
+        id: string;
+    },
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method === 'POST') {
         const session = await getSession({req})
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            //metadata: //
-        })
+
+        console.log(session);
+
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+        let customerId = user.data.stripe_customer_id;
+
+        if(!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                //metadata: //
+            });
+    
+    
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id,
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id;
+        }
+
+
+        // const user = await fauna.query<User>(
+        //     q.Get(
+        //         q.Match(
+        //             q.Index('user_by_email'),
+        //             q.Casefold(session.user.email)
+        //         )
+        //     )
+        // );
+
+        // console.log(user);
+        // console.log(user.ref.id);
+    
+        // await fauna.query(
+        //     q.Update(
+        //         q.Ref(q.Collections('users'), user.ref.id),
+        //         {
+        //             data: {
+        //                 stripe_customer_id: stripeCustomer.id
+        //             }
+        //         }
+        //     )
+        // )
          
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [{
