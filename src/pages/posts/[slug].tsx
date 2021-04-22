0c2a7ed450511fb5f02ce541cee8beb0cc17aa20
Pedro Caires <pedro.caires@iteris.com.br>
2021-04-21 23:17:42 -0300

    fix: posts without session

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,7 +38,7 @@ export const getServerSideProps = async ({req, params}) => {
 
     const { slug } = params;
 
-    if(!session.activeSubscription){
+    if(!session?.activeSubscription){
         return {
             redirect: {
                 destination: '/',
