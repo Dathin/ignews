6ba0417768b51bbcc2d11eb94b6665e370de0f95
Pedro Caires <pedro.caires@iteris.com.br>
2021-04-22 00:07:04 -0300

    fix: logo redirect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 import { ActiveLink } from '../ActiveLink';
+import Link from 'next/link';
 
 export function Header(){
 
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/logo.svg" alt="ig.news" />
+                <Link href="/">
+                    <img src="/logo.svg" alt="ig.news" />                
+                </Link>
                 <nav>
                     <ActiveLink href="/" activeClassName={styles.active}>
                         <a  className={styles.active}>Home</a>
