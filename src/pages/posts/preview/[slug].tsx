b104f1b560a2584e728f9d0081cf11a9174a7411
Pedro Caires <pedro.caires@iteris.com.br>
2021-07-19 02:26:06 -0300

    fix: regenerating slug

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -55,7 +55,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => ({
-  paths: [],
+  paths: [{ params: { slug: "mapas-com-react-usando-leaflet" } }],
   fallback: "blocking", // true, false, blocking
 });
 
